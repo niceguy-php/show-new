618e61abb7db48de434df1408143c41f79040fba
niceguy-php <843409656@qq.com>
2015-04-12 16:09:15 +0800

    modify

---
+++ backend/config/main-local.php
@@ -0,0 +1,42 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'YDune3DgXqPwNY02H37qj02RaFSjD61x',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    //$config['bootstrap'][] = 'debug';
+    //$config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+   // $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii']['class'] = 'yii\gii\Module';
+    $config['modules']['gii']['generators'] = [
+        'kartikgii-crud' => ['class' => 'warrence\kartikgii\crud\Generator'],
+    ];
+//   $config['modules']['gii'] = [
+//        'class' => 'yii\gii\Module',
+//        'generators' => [
+//            'kartikgii-crud' => [
+//                'class' => 'warrence\kartikgii\crud\Generator'
+//            ]
+//        ],
+//    ];
+    /*$config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'generators' => [
+            'mongoDbModel' => [
+                'class' => 'yii\mongodb\gii\model\Generator'
+            ]
+        ],
+    ];*/
+
+}
+
+return $config;

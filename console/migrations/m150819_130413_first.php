fc7946fa2459e169377481b0bcf4e5766e2933aa
niceguy-php <843409656@qq.com>
2015-08-19 23:00:23 +0800

    添加migration

---
+++ console/migrations/m150819_130413_first.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150819_130413_first extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150819_130413_first cannot be reverted.\n";
+
+//        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
